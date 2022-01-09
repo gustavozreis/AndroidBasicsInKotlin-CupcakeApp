694f61489a11ad5cb244b779573f9ead2ea3a5ae
gustavozreis <gzreis@gmail.com>
2022-01-09 19:37:11 -0300

    AJustado string de preço para moeda local

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,12 +3,16 @@ package com.example.cupcake.model
 import android.icu.number.IntegerWidth
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.text.NumberFormat
 import java.util.*
 import java.util.Calendar
 import java.util.Locale
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
 class OrderViewModel: ViewModel() {
 
@@ -29,7 +33,9 @@ class OrderViewModel: ViewModel() {
 
     // preço total
     private val _price = MutableLiveData<Double>()
-    val price: LiveData<Double> = _price
+    val price: LiveData<String> = Transformations.map(_price) {
+        NumberFormat.getCurrencyInstance().format(it)
+    }
 
     init {
         // seta ordens iniciais do pedido
@@ -39,6 +45,7 @@ class OrderViewModel: ViewModel() {
     // setar quantidade
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     // setar sabor do pedido
@@ -49,6 +56,7 @@ class OrderViewModel: ViewModel() {
     // setar data de retirada
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+        updatePrice()
     }
 
     // verificar se o sabor foi escolhido
@@ -75,6 +83,15 @@ class OrderViewModel: ViewModel() {
         _flavor.value = ""
         _date.value = dateOptions[0]
         _price.value = 0.0
+    }
+
+    // definir preço total
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 
 }
